cd1fd7444f91de7dba63731a1a851ef8e60d257d
trian1220 <trian1220@gmail.com>
2024-03-29 23:37:42 +0700

    last commit

--- AplikasiTodolist.java
+++ AplikasiTodolist.java
@@ -3,7 +3,7 @@ public class AplikasiTodolist {
     public static String[] model = new String[10]; 
     public static java.util.Scanner scanner = new java.util.Scanner(System.in);
     public static void main(String[] args) {
-        testViewRemoveTodoList();
+        viewShowTodolist();
     }
 
     // untuk menampilan todolist
